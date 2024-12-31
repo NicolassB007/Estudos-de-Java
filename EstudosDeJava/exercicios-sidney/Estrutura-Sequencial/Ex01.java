f347015ed9ad2d7c4eb8fdf7af2dc7312101250f
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-12-31 19:43:39 -0300

    ..

---
+++ EstudosDeJava/exercicios-sidney/Estrutura-Sequencial/Ex01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner leituraDados = new Scanner(System.in);
+        System.out.print("Primeiro valor: ");
+        double numero = leituraDados.nextDouble();
+
+        System.out.print("Segundo valor: ");
+        double numero2 = leituraDados.nextDouble();
+
+    }
+}
